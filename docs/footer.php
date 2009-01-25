70df4c7866015f77ccd3d7e78723a95623691b22
Michael Baltaks <mbaltaks@gmail.com>
2009-01-26 10:07:38 +1100

    updating copyright year

--- docs/footer.php
+++ docs/footer.php
@@ -67,7 +67,7 @@ generously donated!
 <input type="hidden" name="currency_code" value="USD" />
 <input type="image" src="http://www.paypal.com/en_US/i/btn/x-click-butcc-donate.gif" name="submit" alt="Make payments with PayPal - it's fast, free and secure!" />
 </form>
-Copyright &copy; 2001-2007
+Copyright &copy; 2001-2009
 <a href="mailto:mbaltaks@users.sourceforge.net?subject=DoubleCommand">
 Michael Baltaks</a>
 </div>

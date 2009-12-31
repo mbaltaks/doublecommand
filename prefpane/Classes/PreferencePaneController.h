6f257ac8ceccb5851b9e0140a5c5695348e6178c
Sastira <sastira@gmail.com>
2009-12-30 19:33:41 -0700

    Adding the new prefpane project.

---
+++ prefpane/Classes/PreferencePaneController.h
@@ -0,0 +1,19 @@
+//
+//  prefpanePref.h
+//  prefpane
+//
+//  Created by Tyler Bunnell on 12/30/09.
+//  Copyright (c) 2009 Slightly Sane Inc. All rights reserved.
+//
+
+#import <PreferencePanes/PreferencePanes.h>
+
+
+@interface PreferencePaneController : NSPreferencePane 
+{
+
+}
+
+- (void) mainViewDidLoad;
+
+@end
---
+++ prefpane/Classes/PreferencePaneController.m
@@ -0,0 +1,18 @@
+//
+//  prefpanePref.m
+//  prefpane
+//
+//  Created by Tyler Bunnell on 12/30/09.
+//  Copyright (c) 2009 Slightly Sane Inc. All rights reserved.
+//
+
+#import "PreferencePaneController.h"
+
+
+@implementation PreferencePaneController
+
+- (void) mainViewDidLoad
+{
+}
+
+@end
